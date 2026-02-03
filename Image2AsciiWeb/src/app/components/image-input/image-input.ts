fe7c88df10450513ae03204b58db397fdb9f2183
discovicke <discovicke@gmail.com>
2026-02-03 19:06:59 +0100

    Add ImageInput component with drag-and-drop functionality, file validation, and styles

--- Image2AsciiWeb/src/app/components/image-input/image-input.ts
+++ Image2AsciiWeb/src/app/components/image-input/image-input.ts
@@ -1,11 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DragDropDirective } from '../../directives/drag-drop.directive';
 
 @Component({
   selector: 'app-image-input',
-  imports: [],
-  templateUrl: './image-input.html',
-  styleUrl: './image-input.scss',
+  standalone: true,
+  imports: [CommonModule, DragDropDirective],
+  templateUrl: './image-input.component.html',
+  styleUrl: './image-input.component.scss'
 })
-export class ImageInput {
+export class ImageInputComponent {
+  fileSelected = output<File>();
 
+  protected selectedFileName = '';
+  protected error = '';
+
+  onFileChange(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      this.handleFile(input.files[0]);
+    }
+  }
+
+  onFileDrop(file: File) {
+    this.handleFile(file);
+  }
+
+  private handleFile(file: File) {
+    this.error = '';
+
+    // Validate file type
+    if (!file.type.startsWith('image/')) {
+      this.error = 'Please select an image file';
+      return;
+    }
+
+    // Validate file size (max 5MB)
+    const maxSize = 5 * 1024 * 1024;
+    if (file.size > maxSize) {
+      this.error = 'File size must be less than 5MB';
+      return;
+    }
+
+    this.selectedFileName = file.name;
+    this.fileSelected.emit(file);
+  }
+
+  triggerFileInput() {
+    const input = document.getElementById('file-input') as HTMLInputElement;
+    input?.click();
+  }
 }
