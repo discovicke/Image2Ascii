6bd1819c07c87a54e9d41f6c4cc025e951f63057
discovicke <discovicke@gmail.com>
2026-02-03 18:52:45 +0100

    Add DragDropDirective with drag-and-drop functionality and event handling

--- Image2AsciiWeb/src/app/directives/drag-drop.directive.ts.ts
+++ Image2AsciiWeb/src/app/directives/drag-drop.directive.ts.ts
@@ -1,10 +1,37 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener, HostBinding, output } from '@angular/core';
 
 @Directive({
-  selector: '[appDragDropDirectiveTs]',
+  selector: '[appDragDrop]',
+  standalone: true
 })
-export class DragDropDirectiveTs {
+export class DragDropDirective {
+  fileDropped = output<File>();
 
-  constructor() { }
+  @HostBinding('class.drag-over') dragOver = false;
 
+  @HostListener('dragover', ['$event'])
+  onDragOver(event: DragEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.dragOver = true;
+  }
+
+  @HostListener('dragleave', ['$event'])
+  onDragLeave(event: DragEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.dragOver = false;
+  }
+
+  @HostListener('drop', ['$event'])
+  onDrop(event: DragEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.dragOver = false;
+
+    const files = event.dataTransfer?.files;
+    if (files && files.length > 0) {
+      this.fileDropped.emit(files[0]);
+    }
+  }
 }
