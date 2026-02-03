dd38e26686710a26257683b1ca0796dffed3a053
discovicke <discovicke@gmail.com>
2026-02-03 18:34:44 +0100

    Refactor Program.cs to use minimal hosting model and add Swagger support

--- Image2AsciiApi/Program.cs
+++ Image2AsciiApi/Program.cs
@@ -1,3 +1,10 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAngular", policy =>
@@ -8,4 +15,18 @@ builder.Services.AddCors(options =>
     });
 });
 
+var app = builder.Build();
+
+// Configure the HTTP request pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
 app.UseCors("AllowAngular");
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
