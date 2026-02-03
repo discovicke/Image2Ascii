beb752021aaa4ff978b2fb1f96eb20ed89664c1d
discovicke <discovicke@gmail.com>
2026-02-03 19:03:44 +0100

    Add ImageInput component with template, styles, and unit tests

---
+++ Image2AsciiWeb/src/app/components/image-input/image-input.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-image-input',
+  imports: [],
+  templateUrl: './image-input.html',
+  styleUrl: './image-input.scss',
+})
+export class ImageInput {
+
+}
