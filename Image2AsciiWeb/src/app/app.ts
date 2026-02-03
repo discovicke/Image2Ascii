f49ef78e3cb8527880f499a4d1eec33fd74a7ed4
discovicke <discovicke@gmail.com>
2026-02-03 18:59:44 +0100

    Add app layout with header, footer, and workspace components

--- Image2AsciiWeb/src/app/app.ts
+++ Image2AsciiWeb/src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AsciiService } from './services/ascii-service';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, FooterComponent, WorkspaceComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
