18f694cffc51b5c6a6542aa81959880477cf0c0d
discovicke <discovicke@gmail.com>
2026-02-03 19:25:37 +0100

    Refactor app component to remove unused code and simplify imports

--- Image2AsciiWeb/src/app/app.ts
+++ Image2AsciiWeb/src/app/app.ts
@@ -1,33 +1,13 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { AsciiService } from './services/ascii-service';
+import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { WorkspaceComponent } from './components/workspace/workspace.component';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [HeaderComponent, FooterComponent, WorkspaceComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('Image2AsciiWeb');
-  protected readonly asciiArt = signal('');
-  protected selectedFile: File | null = null;
-  protected width = 100;
-
-  constructor(private asciiService: AsciiService) {}
-
-  onFileSelected(event: any) {
-    this.selectedFile = event.target.files[0];
-  }
-
-  convertImage() {
-    if (this.selectedFile) {
-      this.asciiService.convertImage(this.selectedFile, this.width)
-        .subscribe(response => {
-          this.asciiArt.set(response.ascii);
-        });
-    }
-  }
-}
+export class App {}
