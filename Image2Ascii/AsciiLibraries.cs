b5394edb438965063563f3d329eda43b436a211d
discovicke <discovicke@gmail.com>
2026-02-03 18:06:20 +0100

    Add AsciiLibraries class with predefined ASCII character sets

---
+++ Image2Ascii/AsciiLibraries.cs
@@ -0,0 +1,7 @@
+public static class AsciiLibraries
+{
+    public const string Classic = "@%#*+=-:. ";
+    public const string Detailed = "@$B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. ";
+    public const string Blocks = "█▓▒░@#*+=-:. ";
+    public const string Minimal = "#*. ";
+}
