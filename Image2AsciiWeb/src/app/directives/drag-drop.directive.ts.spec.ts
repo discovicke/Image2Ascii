8ae97180795f51242d0f26cbe6f1a57ebcb3cafc
discovicke <discovicke@gmail.com>
2026-02-03 18:52:08 +0100

    Add DragDropDirective with basic implementation and unit tests

---
+++ Image2AsciiWeb/src/app/directives/drag-drop.directive.ts.spec.ts
@@ -0,0 +1,8 @@
+import { DragDropDirectiveTs } from './drag-drop.directive.ts';
+
+describe('DragDropDirectiveTs', () => {
+  it('should create an instance', () => {
+    const directive = new DragDropDirectiveTs();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ Image2AsciiWeb/src/app/directives/drag-drop.directive.ts.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appDragDropDirectiveTs]',
+})
+export class DragDropDirectiveTs {
+
+  constructor() { }
+
+}
