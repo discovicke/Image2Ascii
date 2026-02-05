2a27020849842112c0348eaadbbba166c6ef59ee
discovicke <discovicke@gmail.com>
2026-02-05 19:34:39 +0100

    Add AsciiLibrary enum and refactor Get method for dynamic library selection

--- Image2Ascii/AsciiLibraries.cs
+++ Image2Ascii/AsciiLibraries.cs
@@ -1,7 +1,21 @@
+public enum AsciiLibrary
+{
+    Classic,
+    Detailed,
+    Blocks,
+    Minimal,
+    Monochrome
+}
+
 public static class AsciiLibraries
 {
-    public const string Classic = "@%#*+=-:. ";
-    public const string Detailed = "@$B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. ";
-    public const string Blocks = "█▓▒░@#*+=-:. ";
-    public const string Minimal = "#*. ";
+    public static string Get(AsciiLibrary lib) => lib switch
+    {
+        AsciiLibrary.Classic => "@%#*+=-:. ",
+        AsciiLibrary.Detailed => "@$B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. ",
+        AsciiLibrary.Blocks => "█▓▒░@#*+=-:. ",
+        AsciiLibrary.Minimal => "#*. ",
+        AsciiLibrary.Monochrome => " .",
+        _ => throw new ArgumentOutOfRangeException(nameof(lib), lib, null)
+    };
 }
