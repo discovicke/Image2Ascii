14145f7acf701f4ea28118e70c24e1c3f5c2335d
discovicke <discovicke@gmail.com>
2026-02-05 19:04:57 +0100

    Rename ConvertImageDTO to ConvertImageDto and improve parameter handling in conversion methods

--- Image2AsciiApi/Models/ConvertImageDTO.cs
+++ Image2AsciiApi/Models/ConvertImageDTO.cs
@@ -2,19 +2,46 @@ using System.Globalization;
 
 namespace Image2AsciiApi.Models;
 
-public class ConvertImageDTO
+public class ConvertImageDto
 {
-    public IFormFile Image { get; set; } = null!;
+    public IFormFile? Image { get; set; }
     public string Width { get; set; } = "100";
     public string Brightness { get; set; } = "0.0";
     public string Gamma { get; set; } = "1.0";
     public string Invert { get; set; } = "false";
 
-    public int GetWidth() => int.TryParse(Width, out var w) ? w : 100;
-    
-    public double GetBrightness() => double.TryParse(Brightness, NumberStyles.Any, CultureInfo.InvariantCulture, out var b) ? b : 0.0;
-    
-    public double GetGamma() => double.TryParse(Gamma, NumberStyles.Any, CultureInfo.InvariantCulture, out var g) ? g : 1.0;
-    
-    public bool GetInvert() => bool.TryParse(Invert, out var i) && i;
+    public int GetWidth()
+    {
+        if (int.TryParse(Width, NumberStyles.Integer, CultureInfo.InvariantCulture, out var w))
+            return w;
+        if (int.TryParse(Width, out w))
+            return w;
+        return 100;
+    }
+
+    public double GetBrightness()
+    {
+        var s = (Brightness ?? "0").Replace(',', '.');
+        if (double.TryParse(s, NumberStyles.Any, CultureInfo.InvariantCulture, out var d))
+            return d;
+        return 0.0;
+    }
+
+    public double GetGamma()
+    {
+        var s = (Gamma ?? "1").Replace(',', '.');
+        if (double.TryParse(s, NumberStyles.Any, CultureInfo.InvariantCulture, out var d))
+            return d;
+        return 1.0;
+    }
+
+    public bool GetInvert()
+    {
+        var s = (Invert ?? "false").Trim();
+        if (bool.TryParse(s, out var b))
+            return b;
+        if (s == "1") return true;
+        if (s == "0") return false;
+        return false;
+    }
 }
