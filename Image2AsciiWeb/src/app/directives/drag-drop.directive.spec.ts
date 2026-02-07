180cb17016f7d660720898690e13065d6fa2e078
discovicke <discovicke@gmail.com>
2026-02-07 17:29:07 +0100

    Refactor ASCII preview component styles for improved padding and layout consistency

--- Image2AsciiWeb/src/app/directives/drag-drop.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DragDropDirectiveTs } from './drag-drop.directive.ts';
-
-describe('DragDropDirectiveTs', () => {
-  it('should create an instance', () => {
-    const directive = new DragDropDirectiveTs();
-    expect(directive).toBeTruthy();
-  });
-});
